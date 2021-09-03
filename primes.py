098a30f96a82c261871fd6a1b30500560ec06bf8
Light Ediand <lightediand@mail.com>
2021-09-03 02:21:03 +0100

    modified:   primes.py

---
+++ primes.py
@@ -0,0 +1,14 @@
+# hi
+
+# this program will generate a list of complex factors of primes
+# the factors will be of the form (a+bi)(a-bi)
+# where a and b belong to L, i is the square root of -1
+# and L is a list that contains squares and "surd squares"
+
+# important definitions
+
+# a square is not a circle
+# a surd is the square root of a prime
+# a surd square is the square of a integer multiple of a surd
+
+
