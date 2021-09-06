c89e04b0b5464a6ff2d1b20fa15129db240e6dfc
Light <45293474+lightediand@users.noreply.github.com>
2021-09-06 05:45:21 +0100

    Update primes.py
    changed the given number to be n

--- primes.py
+++ primes.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 # setting the range
-r = 10
+n = 10
 
 # defining the square
-square = [x*x for x in range(1,r)]
+square = [x*x for x in range(1,n)]
 
 # defining primes
 p = []
 
-for x in range(r):
+for x in range(n):
     if x>1:
         for i in range(2, x):
             if (x % i) ==0:
@@ -21,7 +21,7 @@ for x in range(r):
 surd_square = []
 
 for prime in p:
-    for x in range(1,r):
+    for x in range(1,n):
         surd_square.append(prime*x*x)
 
 # combining the squares and surd squares
