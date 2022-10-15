d3cc65e06144a632ead91f4bab6c0242699ed61d
Light Ediand <lightediand@mail.com>
2022-10-15 23:27:41 +0100

    optimising sieve

--- twosquares.py
+++ twosquares.py
@@ -8,20 +8,13 @@ n = 100
 
 # the loop finds all the products xy within this range
 # these are the composite numbers
-# it first checks if this number falls outside the range
-# it then checks if it has already been removed from the set
-# if the number is in the range and in the set
-# then it is discarded from the set
+# the composites are then discarded from the set 
 
 prime_list = set(range(2,n))
 
 for x in range(2,n):
     for y in range(x,n):
-        if x*y > n:
-            break
-        elif x*y not in prime_list:
-            break
-        else: prime_list.discard(x*y)
+        prime_list.discard(x*y)
         
 
 # defining squares
