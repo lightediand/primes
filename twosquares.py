810a261442d5b571bc3cf52a5dbd1f0618bd7e6c
Liv <>
2022-10-16 15:02:14 +0100

    Use a sieve for generating prime list

--- twosquares.py
+++ twosquares.py
@@ -1,21 +1,20 @@
-# setting the range
+#!/usr/bin/env python3
+
+def prime_sieve(n: int):
+    primes = [True for i in range(n+1)]
+    p = 2
+    while p*p <= n:
+        if (primes[p] == True):
+            for i in range(p*p, n+1, p):
+                primes[i] = False
+        p += 1
+
+    return [x for x, is_prime in enumerate(primes) if is_prime]
+
+
 
 n = 100
-
-# defining primes
-
-# this works by generating a set of all the numbers 2 up to n
-
-# the loop finds all the products xy within this range
-# these are the composite numbers
-# the composites are then discarded from the set 
-
-prime_list = set(range(2,n))
-
-for x in range(2,n):
-    for y in range(x,n):
-        prime_list.discard(x*y)
-        
+prime_list = prime_sieve(n)
 
 # defining squares
 
@@ -34,9 +33,9 @@ for prime in prime_list:
 squares = square + surd_square
 squares.sort()
 
-# writing the primes as the sum of two squares 
+# writing the primes as the sum of two squares
 
-f = open("primes.txt","w") 
+f = open("primes.txt","w")
 
 for prime in prime_list:
     for j in range (len(squares)-1):
