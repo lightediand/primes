cfd99cea74d6e01200f02db3e5bf0aa97924038f
Light Ediand <lightediand@mail.com>
2022-10-16 16:37:58 +0100

    compiled into one program

--- partitions.py
+++ partitions.py
@@ -30,4 +30,20 @@ with open("primes.txt","w") as f:
         for j in range (len(squares)-1):
             for k in range (j, len(squares)):
                 if squares[j] + squares[k] == prime:
-                    f.write("{} = {} + {} \n".format(prime, squares[j], squares[k]))
+                    f.write("{} = {} + {} \n".format(prime, squares[j], squares[k
+
+
+with open("nearsquareprimes.txt","w") as f:
+
+    for prime in prime_list:
+        for j in range (len(square)-1):
+            if square[j] + 1 == prime:
+                f.write("{} = {} + {} \n".format(prime, square[j], 1))
+
+with open("difference.txt","w") as f:
+
+    for prime in prime_list:
+        for j in range(len(prime_list)-1):
+            for k in range(j, len(prime_list)):
+                if prime_list[k] - prime_list[j] == prime:
+                    f.write("{} = {} - {} \n".format(prime, prime_list[k], prime_list[j]))
