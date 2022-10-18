050bef61ce3a584bd3f4125eb485c83956128b19
Light Ediand <lightediand@mail.com>
2022-10-18 23:44:47 +0100

    Removed superfluous loop

--- partitions.py
+++ partitions.py
@@ -9,26 +9,31 @@ n = int(natural_number)
 
 # getting a prime list from our function
 
+print("Starting sieve...")
 prime_list = prime_sieve(n)
+print("Primes obtained!")
 
 # defining squares
-
+print("Getting the squares...")
 square = [x*x for x in range(1,n)]
+print("Squares acquired!")
 
 # defining surd squares
-
+print("Now getting the surd squares which are totally a thing...")
 surd_square = []
 
 for prime in prime_list:
     for x in range(1,n):
         surd_square.append(prime*x*x)
 
+print("We got em!")
+
 # combining the squares and surd squares into one list
 
 squares_incl_surd_squares = square + surd_square
 squares_incl_surd_squares.sort()
 
-# writing the primes as the sum of two squares
+print("Writing primes as the sum of two squares...")
 
 with open("primes.txt","w") as f:
 
@@ -38,6 +43,9 @@ with open("primes.txt","w") as f:
                 if squares_incl_surd_squares[j] + squares_incl_surd_squares[k] == prime:
                     f.write("{} = {} + {} \n".format(prime, squares_incl_surd_squares[j], squares_incl_surd_squares[k]))
 
+print("Done!")
+
+print("Writing primes as the sum of a square and 1...")
 
 with open("nearsquareprimes.txt","w") as f:
 
@@ -46,14 +54,9 @@ with open("nearsquareprimes.txt","w") as f:
             if square[j] + 1 == prime:
                 f.write("{} = {} + {} \n".format(prime, square[j], 1))
 
-with open("difference.txt","w") as f:
+print("Done!")
 
-    for prime in prime_list:
-        for j in range(len(prime_list)-1):
-            for k in range(j, len(prime_list)):
-                if prime_list[k] - prime_list[j] == prime:
-                    f.write("{} = {} - {} \n".format(prime, prime_list[k], prime_list[j]))
-
+print("Writing primes as the difference of a prime and 2...")
 
 with open("twinprimes.txt","w") as f:
 
@@ -61,3 +64,6 @@ with open("twinprimes.txt","w") as f:
             for j in range(len(prime_list)):
                 if prime_list[j] - 2 == prime:
                     f.write("{} = {} - {} \n".format(prime_list[j], prime, 2))
+                    
+print("All done!")
+
